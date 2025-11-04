630596e2c07c1ab3bf2841e742822edd287fb8e3
Manon Wimmer <manonwimmer30@gmail.com>
2025-11-04 10:49:26 +0100

    [ADD] Reset shoot, mine & shockwave value at next frame in controller + tests in tree

--- Assets/Teams/EchoTeam/Scripts/EchoController.cs
+++ Assets/Teams/EchoTeam/Scripts/EchoController.cs
@@ -15,7 +15,14 @@ namespace Echo
 
 		public override InputData UpdateInput(SpaceShipView spaceship, GameData data)
 		{
-			return _inputData;
+			InputData inputDataCopy = _inputData;
+
+			// Reset booleans for next update
+			_inputData.shoot = false;
+			_inputData.dropMine = false;
+			_inputData.fireShockwave = false;
+
+			return inputDataCopy;
 		}
 		
 		public ref InputData GetInputDataByRef() => ref _inputData;
