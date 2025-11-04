bb9d859825d679e92da47c54399be7b11964acac
Haiito92 <hannaantoinebusiness@gmail.com>
2025-11-04 09:33:24 +0100

    [ADD] InputData in EchoController

---
+++ Assets/Teams/EchoTeam/Scripts/Actions/LookAt.cs
@@ -0,0 +1,15 @@
+using BehaviorDesigner.Runtime.Tasks;
+using Echo;
+using UnityEngine;
+
+[TaskCategory("Echo")]
+public class LookAt : Action
+{
+    [SerializeField] private float _targetOrientation;
+
+    public override void OnStart()
+    {
+        EchoController controller = GetComponent<EchoController>();
+        controller.tar
+    }
+}
