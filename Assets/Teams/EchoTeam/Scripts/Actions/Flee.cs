c40f0fe0c463d11bfcbbd55fc70fb2d4ae6e2f70
CorentinRmt <corentin.remot@gmail.com>
2025-11-07 00:00:40 +0100

    [UPDATE] alternative flee ? (to test with everyone)

--- Assets/Teams/EchoTeam/Scripts/Actions/Flee.cs
+++ Assets/Teams/EchoTeam/Scripts/Actions/Flee.cs
@@ -7,8 +7,9 @@ namespace Echo
 {
     public class Flee : EchoAction
     {
-        public SharedFloat checkRadius;
-        public SharedFloat tolerance;
+        public SharedFloat checkRadius = 5f;
+        public SharedFloat tolerance = 0.6f;
+        public SharedFloat angleNeededInvertDirectionFront = 30f;
         private SpaceShipView _ourSpaceShip;
 
         public override void OnAwake()
@@ -32,39 +33,37 @@ namespace Echo
             if (dangerBullet == null)
                 return TaskStatus.Success;
 
-            Vector2 shipToBullet = dangerBullet.Position - _ourSpaceShip.Position;
-            shipToBullet.Normalize();
+            Vector2 bulletToShip = _ourSpaceShip.Position - dangerBullet.Position;
+            bulletToShip.Normalize();
 
-            /*
-            Vector2 shipUp = Vector2.up;
-            float tempX = shipUp.x;
-            float tempY = shipUp.y;
-            shipUp.x = tempX * Mathf.Cos((_ourSpaceShip.Orientation - 90f) * Mathf.Deg2Rad) - tempY * Mathf.Sin((_ourSpaceShip.Orientation - 90f) * Mathf.Deg2Rad);
-            shipUp.y = tempX * Mathf.Sin((_ourSpaceShip.Orientation - 90f) * Mathf.Deg2Rad) + tempY * Mathf.Cos((_ourSpaceShip.Orientation - 90f) * Mathf.Deg2Rad);
+            Vector2 bulletDir = dangerBullet.Velocity.normalized;
+            
+            Vector2 shipUp = EchoMath.Rotate(Vector2.right, _ourSpaceShip.Orientation);
+            Vector2 shipToBullet = -bulletToShip;
 
-            shipUp.Normalize();
-            */
+            // useful to know which direction is better to flee
+            float determinant = bulletDir.x * bulletToShip.y - bulletDir.y * bulletToShip.x;
 
-            float determinant = shipToBullet.x * _ourSpaceShip.Velocity.y - _ourSpaceShip.Velocity.x * shipToBullet.y;
+            // if bullet from behind -> invert direction
+            float scalar = Vector2.Dot(shipUp, shipToBullet);
 
-            float targetOrientation = _ourSpaceShip.Orientation;
-
-            // Bullet goes to the left of the ship -> turn right
-            if (determinant > 0)
+            // If bullet from front but already has enough rotation to flee -> invert direction
+            float alreadyEnoughOrtientationFront = 1f;
+            if (scalar > 0)
             {
-                targetOrientation -= 90f;
-            }
-            // Bullet goes to the right of the ship -> turn left
-            else
-            {
-                targetOrientation += 90f;
+                float angle = EchoMath.OriginToTargetVectorAngle(shipUp, shipToBullet);
+
+                if (angle > angleNeededInvertDirectionFront.Value)
+                {
+                    alreadyEnoughOrtientationFront *= -1f;
+                }
             }
 
-            Vector2 debugUp = Vector2.up;
-            float debugX = debugUp.x;
-            float debugY = debugUp.y;
-            debugUp.x = debugX * Mathf.Cos((targetOrientation - 90f) * Mathf.Deg2Rad) - debugY * Mathf.Sin((targetOrientation - 90f) * Mathf.Deg2Rad);
-            debugUp.y = debugX * Mathf.Sin((targetOrientation - 90f) * Mathf.Deg2Rad) + debugY * Mathf.Cos((targetOrientation - 90f) * Mathf.Deg2Rad);
+            float rotationDir = -Mathf.Sign(determinant) * Mathf.Sign(scalar) * alreadyEnoughOrtientationFront;
+
+            float targetOrientation = _ourSpaceShip.Orientation + 90f * rotationDir;
+
+            Vector2 debugUp = EchoMath.Rotate(Vector2.right, targetOrientation);
 
             Debug.DrawLine(_ourSpaceShip.Position, _ourSpaceShip.Position + debugUp * 5f, Color.green, 1f);
 
